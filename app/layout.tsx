d1cef48bec9dd0484ca32da821f4e30cf5ba1668
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-17 13:43:27 -0400

    fresh project

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,13 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+            <div className="justify-top flex min-h-screen w-full max-w-3xl flex-col items-center gap-8 bg-white px-16 py-16 sm:items-start dark:bg-black">
+            {children}
+            </div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
