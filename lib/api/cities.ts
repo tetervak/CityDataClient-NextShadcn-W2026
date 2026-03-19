97209da97feaef08340ee59cb795bcc41724be9a
Alexander Tetervak <tetervak@sheridancollege.ca>
2026-03-19 16:05:09 -0400

    replaced fetch with axios.get

--- lib/api/cities.ts
+++ lib/api/cities.ts
@@ -1,11 +1,12 @@
+import axios from "axios"
+import { City } from "@/lib/api/types"
+
 export async function fetchCities() {
-  const res = await fetch("http://localhost:8080/api/cities")
-  if (!res.ok) throw new Error("Failed to fetch cities")
-  return res.json()
+  const res = await axios.get<City[]>("http://localhost:8080/api/cities")
+  return res.data
 }
 
 export async function fetchCity(id: string) {
-  const res = await fetch(`http://localhost:8080/api/cities/${id}`)
-  if (!res.ok) throw new Error("Failed to fetch city")
-  return res.json()
+  const res = await axios.get<City>(`http://localhost:8080/api/cities/${id}`)
+  return res.data
 }
