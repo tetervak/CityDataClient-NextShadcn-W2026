b9b987795de2d6fede15a361904f1de951b7de72
Alex Tetervak <tetervak@sheridancollege.ca>
2026-03-25 17:13:39 -0400

    added crud api

--- lib/api/cities.ts
+++ lib/api/cities.ts
@@ -1,12 +1,51 @@
 import axios from "axios"
 import { City } from "@/lib/api/types"
 
+function dataUrl(id?: string): string{
+  if(id){
+    return `http://localhost:8080/api/cities/${id}`;
+  }else{
+    return 'http://localhost:8080/api/cities';
+  }
+}
+
 export async function fetchCities(): Promise<City[]> {
-  const res = await axios.get<City[]>("http://localhost:8080/api/cities")
+  const res = await axios.get<City[]>(dataUrl())
   return res.data
 }
 
 export async function fetchCity(id: string): Promise<City> {
-  const res = await axios.get<City>(`http://localhost:8080/api/cities/${id}`)
+  const res = await axios.get<City>(dataUrl(id))
   return res.data
+}
+
+export async function deleteCity(id: string): Promise<void> {
+  const res = await axios.delete(dataUrl(id))
+  return res.data
+}
+
+export async function addCity(city: City): Promise<City> {
+  const res = await axios.post(
+    dataUrl(),
+    city,
+    {
+      headers: { "Content-Type": "application/json" }
+    }
+  )
+
+  return res.data
+}
+
+export async function updateCity(city: City): Promise<City> {
+  const response = await axios.put(
+    dataUrl(city.cityId),
+    city,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  )
+
+  return response.data
 }
